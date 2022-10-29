eba377383b5f90783591d23edc6dda9ae3b5f483
moefsaadi <moefsaadi@gmail.com>
2022-10-28 21:15:37 -0400

    Instantiated Bottom Navigation View Functionality

--- app/src/main/java/com/example/healthassistant/activities/HomeActivity.kt
+++ app/src/main/java/com/example/healthassistant/activities/HomeActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import com.example.healthassistant.R
 
 class HomeActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
