1189ee516711da4f3324effa3cab9875a784d112
moefsaadi <moefsaadi@gmail.com>
2022-10-31 13:41:08 -0400

    Upgraded to SDK 33, Added ViewBinding, Added BotNav UI

--- app/src/main/java/com/example/healthassistant/activities/HomeActivity.kt
+++ app/src/main/java/com/example/healthassistant/activities/HomeActivity.kt
@@ -3,12 +3,39 @@ package com.example.healthassistant.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.healthassistant.R
+import com.example.healthassistant.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottomNavBar.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.alarm_menu ->
+                {
+                    //TODO: Navigate to Alarm Page
+                    true
+                }
+                R.id.home_menu ->
+                {
+                    //TODO: Navigate to Home Page
+                    true
+                }
+                R.id.settings_menu ->
+                {
+                    //TODO: Navigate to Settings Page
+                    true
+                }
+                else -> false
+
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/healthassistant/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/healthassistant/activities/SettingsActivity.kt
@@ -1,4 +1,6 @@
 package com.example.healthassistant.activities
 
-class SettingsActivity {
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
 }
