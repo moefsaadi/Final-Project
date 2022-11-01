53d101aa8cf5ab330718f60d91b27bc1b9e50875
moefsaadi <moefsaadi@gmail.com>
2022-11-01 11:47:14 -0400

    Added 3 dot button, and Snackbar

--- app/src/main/java/com/example/healthassistant/activities/HomeActivity.kt
+++ app/src/main/java/com/example/healthassistant/activities/HomeActivity.kt
@@ -1,12 +1,18 @@
 package com.example.healthassistant.activities
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.healthassistant.R
 import com.example.healthassistant.databinding.ActivityHomeBinding
+import com.google.android.material.snackbar.Snackbar
 
 class HomeActivity : AppCompatActivity() {
 
@@ -23,15 +29,22 @@ class HomeActivity : AppCompatActivity() {
                 R.id.alarm_menu ->
                 {
                     //TODO: Navigate to Alarm Page
+
+                    snackbarCreate("Switched To Alarm Page","Dismiss")
+
                     true
                 }
                 R.id.home_menu ->
                 {
+                    snackbarCreate("Switched To Home Page","Dismiss")
+
                     //TODO: Navigate to Home Page
                     true
                 }
                 R.id.settings_menu ->
                 {
+                    snackbarCreate("Switched To Settings Page","Dismiss")
+
                     //TODO: Navigate to Settings Page
                     true
                 }
@@ -48,17 +61,16 @@ class HomeActivity : AppCompatActivity() {
 
                 binding.fabAppt.startAnimation(fabCloseAnim)
                 binding.fabMed.startAnimation(fabCloseAnim)
-                binding.tvAppt.visibility = View.INVISIBLE
-                binding.tvMed.visibility = View.INVISIBLE
+                binding.tvAppt.startAnimation(fabCloseAnim)
+                binding.tvMed.startAnimation(fabCloseAnim)
 
                 isOpen = false
             } else {
 
-
                 binding.fabAppt.startAnimation(fabOpenAnim)
                 binding.fabMed.startAnimation(fabOpenAnim)
-                binding.tvAppt.visibility = View.VISIBLE
-                binding.tvMed.visibility = View.VISIBLE
+                binding.tvAppt.startAnimation(fabOpenAnim)
+                binding.tvMed.startAnimation(fabOpenAnim)
 
                 isOpen = true
             }
@@ -71,5 +83,41 @@ class HomeActivity : AppCompatActivity() {
         binding.fabAppt.setOnClickListener{
             //TODO: Write instructions
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val lightTxt = "Switching to Light Theme!"
+        val darkTxt = "Switching to Dark Theme!"
+
+        when(item.itemId){
+            R.id.light_theme_menu ->
+            {
+                snackbarCreate("Switched To Light Mode","Dismiss")
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            R.id.dark_theme_menu ->
+            {
+                snackbarCreate("Switched To Dark Mode","Dismiss")
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
+
+        return true
+    }
+
+    private fun snackbarCreate(message: String, action: String)
+    {
+        val snackbar = Snackbar.make(binding.snackbarLayout,message,Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction(action){
+            //Dismiss
+        }
+        snackbar.anchorView = binding.bottomNavBar
+        snackbar.show()
     }
 }
